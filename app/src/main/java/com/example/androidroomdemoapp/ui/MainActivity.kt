bd52a502c9b5d380ca4a0435b028ae57d59f3979
Ravi Pandya <pandya@smooth.tech>
2022-01-13 17:29:48 +0530

    Implement a view and add SubscriberRepository.kt

--- app/src/main/java/com/example/androidroomdemoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidroomdemoapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.androidroomdemoapp
+package com.example.androidroomdemoapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidroomdemoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
